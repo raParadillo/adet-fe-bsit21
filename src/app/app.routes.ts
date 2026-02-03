04aae5bf254ce75ea6a12dc3228b4108c3d9c875
Raque Alexy Paradillo <raparadillo06235@liceo.edu.ph>
2026-02-03 10:30:29 +0800

    community

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
-import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Routes } from '@angular/router';
+import { PostsComponent } from './posts/posts.component';
+
+export const routes: Routes = [
+  { path: 'posts', component: PostsComponent }
+];
